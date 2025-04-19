260d3f5098c927d131c9386e916b163738b7055d
swz-git <contact@swz.works>
2025-04-19 19:39:07 +0200

    use set-gravity command instead of manually applying it

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,14 @@
 use rlbot::{
     Packet, RLBotConnection,
-    flat::{self, DesiredBallState, DesiredCarState},
+    flat::{ConnectionSettings, ConsoleCommand, DesiredGameState, MatchPhase},
     util::RLBotEnvironment,
 };
 
-struct ZeroGScript {
-    last_game_time: f32,
-}
+// const TARGET_GRAVITY: f32 = -f32::MIN_POSITIVE;
+const TARGET_GRAVITY: f32 = -2000.0;
 
-impl rlbot::scripts::Script for ZeroGScript {
-    fn new(
-        _agent_id: String,
-        _match_configuration: rlbot::flat::MatchConfiguration,
-        _field_info: rlbot::flat::FieldInfo,
-        _packet_queue: &mut rlbot::util::PacketQueue,
-    ) -> Self {
-        Self {
-            last_game_time: 0.0,
-        }
-    }
-
-    fn tick(
-        &mut self,
-        game_packet: rlbot::flat::GamePacket,
-        packet_queue: &mut rlbot::util::PacketQueue,
-    ) {
-        let delta_time = game_packet.match_info.seconds_elapsed - self.last_game_time;
-        self.last_game_time = game_packet.match_info.seconds_elapsed;
-        if game_packet.balls.len() == 0 {
-            return;
-        }
-        packet_queue.push(Packet::DesiredGameState(flat::DesiredGameState {
-            ball_states: game_packet
-                .balls
-                .iter()
-                .map(|ball| {
-                    let mut physics = ball.physics;
-                    physics.velocity.z += 650.0 * delta_time;
-                    DesiredBallState {
-                        physics: Box::new(physics.into()),
-                    }
-                })
-                .collect(),
-            car_states: game_packet
-                .players
-                .iter()
-                .map(|player| {
-                    let mut physics = player.physics;
-                    physics.velocity.z += 650.0 * delta_time;
-                    DesiredCarState {
-                        physics: Some(Box::new(physics.into())),
-                        boost_amount: None,
-                    }
-                })
-                .collect(),
-            match_info: None,
-            console_commands: vec![],
-        }));
-    }
-}
+const COMMAND_REPEAT_INTERVAL: f32 = 0.05; // seconds
+const COMMAND_REPEAT_DURATION: f32 = 0.5; // seconds
 
 fn main() {
     let RLBotEnvironment {
@@ -66,11 +16,58 @@ fn main() {
         agent_id,
     } = RLBotEnvironment::from_env();
     let agent_id = agent_id.unwrap_or_else(|| "swz/zero-g-script".into());
-    let rlbot_connection = RLBotConnection::new(&server_addr).expect("connection");
+    let mut rlbot_connection = RLBotConnection::new(&server_addr).expect("connection");
 
-    // Blocking.
-    rlbot::scripts::run_script::<ZeroGScript>(agent_id.clone(), true, true, rlbot_connection)
-        .expect("run_script crashed");
+    rlbot_connection
+        .send_packet(ConnectionSettings {
+            agent_id: agent_id.clone(),
+            close_between_matches: true,
+            ..Default::default()
+        })
+        .expect("failed to send ConnectionSettings");
+
+    rlbot_connection
+        .send_packet(Packet::InitComplete)
+        .expect("failed to send InitComplete");
+
+    let mut prev_match_phase = MatchPhase::Inactive;
+    let mut last_kickoff_time = 0.0;
+    let mut last_application_time = 0.0;
+    while let Ok(packet) = rlbot_connection.recv_packet() {
+        if let Packet::GamePacket(game) = &packet {
+            if game.match_info.match_phase == MatchPhase::Kickoff
+                && prev_match_phase == MatchPhase::Countdown
+            {
+                last_kickoff_time = game.match_info.seconds_elapsed;
+                println!("ZERO-G: Applying zero-g");
+            }
+            prev_match_phase = game.match_info.match_phase;
+
+            // Repeat zero-g command for x seconds
+            if game.match_info.seconds_elapsed - last_kickoff_time > COMMAND_REPEAT_DURATION {
+                continue;
+            }
+
+            // Apply zero-g command every x seconds
+            if game.match_info.seconds_elapsed - last_application_time >= COMMAND_REPEAT_INTERVAL {
+                last_application_time = game.match_info.seconds_elapsed;
+                rlbot_connection
+                    .send_packet(DesiredGameState {
+                        ball_states: vec![],
+                        car_states: vec![],
+                        match_info: None,
+                        console_commands: vec![ConsoleCommand {
+                            command: format!("Set WorldInfo WorldGravityZ {}", TARGET_GRAVITY),
+                        }],
+                    })
+                    .expect("failed to send set gravity command");
+            }
+        }
+        // idle until connection is closed
+        if let Packet::None = packet {
+            break;
+        }
+    }
 
     println!("Script with agent_id `{agent_id}` exited nicely");
 }
